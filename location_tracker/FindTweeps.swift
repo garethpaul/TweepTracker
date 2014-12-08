b94904201aa9af177c38c5fdc9e3e79b2a2da5e1
Gareth Paul Jones <gareth@garethpaul.com>
2014-12-07 22:27:40 -0800

    adding updates to Finding Tweeps

--- location_tracker/FindTweeps.swift
+++ location_tracker/FindTweeps.swift
@@ -1,5 +1,5 @@
 //
-//  FindPictures.swift
+//  FindTweeps.swift
 //
 
 import Foundation
@@ -11,68 +11,84 @@ func FindTweeps(completion: (result: [String]) -> Void) {
     typealias JSONDictionary = Dictionary<String, JSON>
     typealias JSONArray = Array<JSON>
 
-    let statusesShowEndpoint = "https://api.twitter.com/1.1/lists/members.json"
+    // We must get the memebers of the list.
+    // As a pointer ou can find the members of via :- https://twitter.com/TwitterDev/lists/twitter-dev-advocates
+    // Documentation on this endpoint can be found via :- https://dev.twitter.com/rest/reference/get/lists/members
+    //
+
+    // Setup endpoint URL
+    let memberListAPI = "https://api.twitter.com/1.1/lists/members.json"
+
+
+    // Setup Params to be sent to the endpoint.
     let params = ["slug": "twitter-dev-advocates", "owner_screen_name": "twitterdev"]
+
+    // Setup a catch for errors.
     var clientError : NSError?
+
+    // initialize Twitter
     Twitter.initialize()
-    let completed = false
 
-    Twitter.sharedInstance().logInWithCompletion{
-        (session, error) -> Void in
-        if (session != nil) {
-            //println("signed in as \(session.userName)");
-            /// go
-            let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod("GET", URL:  statusesShowEndpoint, parameters: params, error:&clientError)
+    // Create a request to the API notice "GET", "URL" params.
+    let request = Twitter.sharedInstance().APIClient.URLRequestWithMethod("GET", URL:  memberListAPI, parameters: params, error:&clientError)
 
-            if request != nil {
-                println("request is not nil")
-                Twitter.sharedInstance().APIClient.sendTwitterRequest(request) {
-                    (response, data, connectionError) -> Void in
-                    if (connectionError == nil) {
-                        var jsonError : NSError?
-                        let json : AnyObject? =
-                        NSJSONSerialization.JSONObjectWithData(data,
-                            options: nil,
-                            error: &jsonError)
+    // We only want to do things when the request is not nill
+    if request != nil {
 
-                        // users
-                        var userArray = Array<String>()
-                        if let users = json!["users"] as? JSONArray {
-                               for user in users {
-                                if let screenName = user["screen_name"] as?String {
-                                    userArray.append(screenName)
-                                }
-                            }
+        // The API call is ready to send.
+        Twitter.sharedInstance().APIClient.sendTwitterRequest(request) {
+            (response, data, connectionError) -> Void in
+
+            // If there are no errors
+            if (connectionError == nil) {
+                var jsonError : NSError?
+                let json : AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError)
+
+                // userArray this is how we store all the users to send back to the ViewController.
+                var userArray = Array<String>()
+
+                // get the output from the json {"users": [...]}
+                if let users = json!["users"] as? JSONArray {
+
+                    // iterate through the list of users
+                    for user in users {
+
+                        // if there is a json object like the following {"screen_name": "gpj"}
+                        if let screenName = user["screen_name"] as?String {
+
+                            // Append the user the array
+                            userArray.append(screenName)
                         }
-                        // manually add some
-                        userArray.append("jcipriano")
-                        userArray.append("joncipriano")
-                        userArray.append("davelester")
-                        userArray.append("lfcipriani")
-                        userArray.append("ktopenn")
-                        userArray.append("logicalarthur")
-                        userArray.append("laurenschutte")
-                        userArray.append("beardigsit")
-                        userArray.append("noonisms")
-                        userArray.append("isaach")
-                        completion(result: userArray)
-                    }
-
-                    else {
-                        println("Error: \(connectionError)")
                     }
                 }
-            }
-            else {
-                println("Error: \(clientError)")
+
+                // The list of developer advocates is somewhat limited here are some more to append to our array
+                userArray.append("jcipriano")
+                userArray.append("joncipriano")
+                userArray.append("davelester")
+                userArray.append("lfcipriani")
+                userArray.append("ktopenn")
+                userArray.append("logicalarthur")
+                userArray.append("laurenschutte")
+                userArray.append("beardigsit")
+                userArray.append("noonisms")
+
+                // On completed send the userArray back to the ViewController.
+                completion(result: userArray)
             }
 
-        } else {
-            println("error: \(error.localizedDescription)");
+            else {
+                println("Error: \(connectionError)")
+            }
         }
-        
     }
+    else {
+        println("Error: \(clientError)")
+    }
+
 }
+
+
 
 
 
